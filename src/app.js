d91dfc84ee775c962dc6227b303c357a43bd018e
MNDZ19 <jorgemorenomendez19@gmail.com>
2025-04-26 13:45:01 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,47 @@ import "bootstrap";
 import "./style.css";
 
 
+
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.us', '.es'];
+
+  let domains = []; // Aquí vamos a guardar todos los dominios posibles
+
+  // Generar todas las combinaciones y guardarlas en el array
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let ext of extensions) {
+          domains.push(`${p}${a}${n}${ext}`);
+        }
+      }
+    }
+  }
+
+  let domainList = document.getElementById('domainList');
+  let generateBtn = document.getElementById('generateBtn');
+
+  let currentIndex = 0;
+
+  // Cuando el usuario hace click en el botón
+  generateBtn.addEventListener('click', function () {
+    if (currentIndex < domains.length) {
+      let listItem = document.createElement('li');
+      listItem.textContent = domains[currentIndex];
+      domainList.appendChild(listItem);
+      currentIndex++;
+    } else {
+      alert('¡Ya no hay más dominios para mostrar!');
+    }
+  });
+
 };
